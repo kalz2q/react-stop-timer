b6977c9450c952df947d53f8cdbb686b520bfab4
kalz2q <katsu95i@gmail.com>
2022-08-25 05:26:49 +0900

    requestAnimationFrame

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [stoppedTime, setStoppedTime] = useState(0);
+  const [time, setTime] = useState(0);
+  // const [lastStartedAt, setLastStartedAt] = useState(null);
+  const [lastStartedAt, setLastStartedAt] = useState(0);
+
+  const calcTime = () => {
+    // return lastStartedAt === null
+    return lastStartedAt === 0
+      ? stoppedTime
+      : stoppedTime + new Date().getTime() - lastStartedAt;
+  };
+
+  const onClick = () => {
+    // if (lastStartedAt === null) {
+    if (lastStartedAt === 0) {
+      setLastStartedAt(new Date().getTime());
+      return;
+    }
+    setStoppedTime(calcTime());
+    // setLastStartedAt(null);
+    setLastStartedAt(0);
+    return;
+  };
+
+  // useEffect(() => {
+  //   const requestId = requestAnimationFrame(() => setTime(calcTime()));
+  //   return () => cancelAnimationFrame(requestId);
+  // });
+
+  requestAnimationFrame(() => setTime(calcTime()));
+  // setInterval(() => setTime(calcTime()));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>timer (sec): {Math.floor(time / 1000)}</p>
+      <p>timer (ms): {time}</p>
+      <p>
+        <button onClick={onClick}>
+          {/* {lastStartedAt === null ? "start" : "stop"} */}
+          {lastStartedAt === 0 ? "start" : "stop"}
+        </button>
+      </p>
     </div>
   );
 }
